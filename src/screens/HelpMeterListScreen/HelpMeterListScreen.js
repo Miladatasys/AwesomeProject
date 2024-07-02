1f5e3fa6c94d9ac763647f9101124a8841b97a45
Samira Awad Labarca <136211595+samirabawad@users.noreply.github.com>
2024-07-01 20:34:18 -0400

    se arregla diseño meterlistscreen

--- src/screens/HelpMeterListScreen/HelpMeterListScreen.js
+++ src/screens/HelpMeterListScreen/HelpMeterListScreen.js
@@ -19,7 +19,7 @@ const HelpMeterListScreen = () => {
                     throw new Error('No token found');
                 }
                 const response = await axios.get('http://192.168.1.88:8080/cliente/userMedidores/profile', {
-                    headers: { Authorization: `Bearer ${token}` }
+                  headers: { Authorization: `Bearer ${token}` }
                 });
                 if (response.data && response.data.object) {
                     setMeters(response.data.object);
@@ -139,4 +139,3 @@ const styles = StyleSheet.create({
 });
 
 export default HelpMeterListScreen;
-
--- src/screens/MeterListScreen/MeterListScreen.js
+++ src/screens/MeterListScreen/MeterListScreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert  } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Alert, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ClientHeader from '../../components/CustomHeader/ClientHeader';
 
 const MeterListScreen = () => {
     const navigation = useNavigation();
@@ -27,6 +28,7 @@ const MeterListScreen = () => {
                 }
             } catch (error) {
                 console.error('Error fetching meters', error);
+                Alert.alert('Error', 'Hubo un problema al cargar los medidores');
             } finally {
                 setLoading(false);
             }
@@ -49,42 +51,45 @@ const MeterListScreen = () => {
                 console.log('response.data.fecha:', response.data.fecha);
                 const responseFecha = response.data.fecha;
                 const diaObtenido = parseInt(responseFecha.split('-')[2]);
-                const diaAyer = diaObtenido -1;
-                const diaTomorrow = diaObtenido +1;
+                const diaAyer = diaObtenido - 1;
+                const diaTomorrow = diaObtenido + 1;
 
                 console.log('response', response.data.fecha);
                 console.log('diaObtenido: ', diaObtenido);
                 console.log('diaAyer: ', diaAyer);
                 console.log('diaTomorrow: ', diaTomorrow);
 
-                const fecha_actual =new Date().toISOString().split('T')[0];
-                console.log('Fecha de hoy',fecha_actual);
+                const fecha_actual = new Date().toISOString().split('T')[0];
+                console.log('Fecha de hoy', fecha_actual);
 
                 const diaActual = parseInt(fecha_actual.split('-')[2]);
                 console.log('diaActual: ', diaActual);
 
                 const diaHoy = parseInt(fecha_actual.split('-')[2]);
-                console.log('dia hoy',diaHoy)
+                console.log('dia hoy', diaHoy);
 
                 if (diaActual === diaAyer || diaActual === diaTomorrow || diaActual === diaObtenido) {
                     console.log('Navigating to CameraScreen');
-                  navigation.navigate('CameraScreen', { meter, fecha: response.data.fecha });
+                    navigation.navigate('CameraScreen', { meter, fecha: response.data.fecha });
                 } else {
-                  console.log('Date mismatch, showing alert');
-                  Alert.alert('Aviso', `No corresponde la fecha para tomar la foto. Su fecha es el día ${diaObtenido} de cada mes`);
+                    console.log('Date mismatch, showing alert');
+                    Alert.alert('Aviso', `No corresponde la fecha para tomar la foto. Su fecha es el día ${diaObtenido} de cada mes`);
                 }
-              } else {
+            } else {
                 Alert.alert('Aviso', response.data.message || 'No corresponde la fecha para tomar la foto.');
-              }
+            }
         } catch (error) {
             console.error('Error checking fecha consumo', error);
             Alert.alert('Error', 'Hubo un problema al verificar la fecha de consumo');
         }
     };
 
-
     if (loading) {
-        return <Text>Cargando...</Text>;
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
     }
 
     return (
@@ -94,15 +99,30 @@ const MeterListScreen = () => {
                 <Text style={styles.backButtonText}>Volver</Text>
             </TouchableOpacity>
             <Text style={styles.title}>Seleccione un Medidor</Text>
-            <FlatList
-                data={meters}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                    <TouchableOpacity style={styles.meterItem} onPress={() => handleMeterSelect(item)}>
-                        <Text style={styles.meterText}>{item.direccion}</Text>
-                    </TouchableOpacity>
+            <ScrollView>
+                {meters.length === 0 ? (
+                    <View style={styles.noMedidoresContainer}>
+                        <Text style={styles.noMedidoresText}>No tienes medidores. Registra uno.</Text>
+                    </View>
+                ) : (
+                    meters.map((meter, index) => (
+                        <View key={index} style={styles.medidorRow}>
+                            <View style={styles.medidorContainer}>
+                                <ClientHeader
+                                    direccion={{
+                                        calle: meter.direccion,
+                                        comuna: meter.comuna,
+                                        region: meter.region,
+                                        numeroCliente: meter.numcliente,
+                                    }}
+                                    medidorId={meter.id}
+                                    onPress={() => handleMeterSelect(meter)}
+                                />
+                            </View>
+                        </View>
+                    ))
                 )}
-            />
+            </ScrollView>
         </View>
     );
 };
@@ -112,22 +132,6 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         backgroundColor: '#FFFFFF',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        color: '#333333',
-    },
-    meterItem: {
-        padding: 15,
-        backgroundColor: '#F2F2F2',
-        borderRadius: 8,
-        marginBottom: 10,
-    },
-    meterText: {
-        fontSize: 18,
-        color: '#333333',
     },
     backButtonContainer: {
         flexDirection: 'row',
@@ -140,6 +144,42 @@ const styles = StyleSheet.create({
         marginLeft: 5,
         fontWeight: 'bold',
         fontFamily: 'Roboto-Regular',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        marginTop: 20,
+        color: '#333333',
+    },
+    noMedidoresContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    noMedidoresText: {
+        fontSize: 18,
+        color: '#FF0000',
+        textAlign: 'center',
+    },
+    medidorRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    medidorContainer: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
 
